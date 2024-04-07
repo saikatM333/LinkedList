a36485c232996ccaf3407ea26b7025dd5083c0e0
saikatCV <saikat030703@gmail.com>
2024-04-07 10:11:56 +0530

    added the generic node class

--- LinkedList/LinkedListImplementation.cs
+++ LinkedList/LinkedListImplementation.cs
@@ -3,14 +3,21 @@
 using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 
+public class node<T>
+{  
+    public T Data { get; set; }
+    public node<T> Next { get; set; }
+public node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
 
-    public  class LinkedListImplementation
+public class LinkedListImplementation
 {
     public  static void Main(string[] args)
     {
         Console.WriteLine("Linked list implemntation ");
-
-        
-
     }
 }
